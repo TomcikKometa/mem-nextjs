35b11228fe8e4d13f2cb95ad4b8b4fadce2474a5
TomcikKometa <tomcik48_84@o2.pl>
2024-07-02 16:47:15 +0200

    change name of files

--- src/app/pages/mem-card/page.jsx
+++ src/app/pages/mem-card/page.jsx
@@ -8,54 +8,63 @@ const data = [
     upvotes: 6,
     downvotes: 0,
     img: "1.jpg",
+    id:0
   },
   {
     title: "Mem 2",
     upvotes: 1,
     downvotes: 2,
     img: "2.jpg",
+    id:1
   },
   {
     title: "Mem 3",
     upvotes: 6,
     downvotes: 0,
     img: "3.jpg",
+    id:2
   },
   {
     title: "Mem 4",
     upvotes: 1,
     downvotes: 2,
     img: "4.jpg",
+    id:3
   },
   {
     title: "Mem 5",
     upvotes: 6,
     downvotes: 0,
     img: "5.jpg",
+    id:4
   },
   {
     title: "Mem 6",
     upvotes: 1,
     downvotes: 2,
     img: "6.jpg",
+    id:5
   },
   {
     title: "Mem 7",
     upvotes: 1,
     downvotes: 2,
     img: "7.jpg",
+    id:6
   },
   {
     title: "Mem 8",
     upvotes: 1,
     downvotes: 2,
     img: "8.jpg",
+    id:7
   },
   {
     title: "Mem 9",
     upvotes: 1,
     downvotes: 2,
     img: "9.jpg",
+    id:8
   },
 ];
 
