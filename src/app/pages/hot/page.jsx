48cd5f5b2dd9b4ae694ceae28878759388327fdd
TomcikKometa <tomcik48_84@o2.pl>
2024-07-05 12:51:27 +0200

    rafactor

---
+++ src/app/pages/hot/page.jsx
@@ -0,0 +1,20 @@
+"use client";
+import store from "../../store/store";
+import { useStore,useSelector } from "react-redux";
+import MemList from "../../components/mem-card/MemList";
+
+export default function Mems() {
+  let  showMemList =  useStore((state) => state.mems);
+  const showMemLists =  useSelector((state) => state.mems);
+  showMemList.subscribe(() => console.log(useStore((state) => state.mems)))
+  
+  let x = showMemList.getState()
+  console.log(showMemList.getState())
+
+  
+    
+
+  return <MemList memList={x.mems.all}/>;
+  
+}
+
