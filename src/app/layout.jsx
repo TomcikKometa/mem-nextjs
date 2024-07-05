8995d0d2ede2b0a3b01fceb29bfc109666052a65
TomcikKometa <tomcik48_84@o2.pl>
2024-07-05 15:40:07 +0200

    instal persist store

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,14 +2,27 @@ import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import { Providers } from "./store/providers";
 import { store } from "./store/store";
+import Link from 'next/link'
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <Providers store={store}>
-          <Navbar />
-          <main>{children}</main>
+        {/* <BrowserRouter> */}
+        <Navbar />
+        <main>
+          {/* <Routes>
+            <Route path="/" element={<ProfilePage />} />
+            <Route path="/chat" element={<ChatPage />} />
+            <Route path="*" element={<ErrorPage />} /> */}
+            <main>{children}</main>
+          {/* </Routes> */}
+        </main>
+      {/* </BrowserRouter> */}
+          
+          
         </Providers>
       </body>
     </html>
