4d454584b184bd5a12ae96982ac613b7e823afbe
TomcikKometa <tomcik48_84@o2.pl>
2024-07-07 01:26:28 +0200

    change log info

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
  export default function Home() {
   return (
-    <main className="flex p-8 flex-col items-center justify-between text-7xl overflow-hidden">
-      <p>Szukajcie a znajdziecie... ...memów</p>
+    <main className="flex p-8 flex-col items-center justify-between text-5xl overflow-hidden">
+      <p>Witaj w aplikacji do głosownia na ulubione memy</p>
       <Image
                       src="/face-pack-person-svgrepo-com.svg"
                       alt="Picture of the author"
                       width={350}
                       height={350}
-                      className="object-cover object-top rounded-sm img_shadow mt-10 pt-5 p-2"
+                      className="object-cover object-top rounded-sm img_shadow mt-10 pt-10 p-2"
                     />
     </main>
   );
