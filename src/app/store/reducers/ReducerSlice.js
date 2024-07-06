7590efc33be06594dd07803bed855dbcb5555a8d
TomcikKometa <tomcik48_84@o2.pl>
2024-07-07 01:31:10 +0200

    refacotr store

--- src/app/store/reducers/ReducerSlice.js
+++ src/app/store/reducers/ReducerSlice.js
@@ -1,72 +1,6 @@
 "use client";
 import { createSlice } from "@reduxjs/toolkit";
 
-let list = [
-  {
-    title: "Mem 1",
-    upvotes: 6,
-    downvotes: 0,
-    img: "1.jpg",
-    id: 0,
-  },
-  {
-    title: "Mem 2",
-    upvotes: 1,
-    downvotes: 2,
-    img: "2.jpg",
-    id: 1,
-  },
-  {
-    title: "Mem 3",
-    upvotes: 6,
-    downvotes: 0,
-    img: "3.jpg",
-    id: 2,
-  },
-  {
-    title: "Mem 4",
-    upvotes: 1,
-    downvotes: 2,
-    img: "4.jpg",
-    id: 3,
-  },
-  {
-    title: "Mem 5",
-    upvotes: 6,
-    downvotes: 0,
-    img: "5.jpg",
-    id: 4,
-  },
-  {
-    title: "Mem 6",
-    upvotes: 1,
-    downvotes: 2,
-    img: "6.jpg",
-    id: 5,
-  },
-  {
-    title: "Mem 7",
-    upvotes: 1,
-    downvotes: 2,
-    img: "7.jpg",
-    id: 6,
-  },
-  {
-    title: "Mem 8",
-    upvotes: 1,
-    downvotes: 2,
-    img: "8.jpg",
-    id: 7,
-  },
-  {
-    title: "Mem 9",
-    upvotes: 1,
-    downvotes: 2,
-    img: "9.jpg",
-    id: 8,
-  },
-];
-
 let stateList = {
   all: [
     {
@@ -137,13 +71,14 @@ let stateList = {
   hot: [],
 };
 
-const hotList = []
+const hotList = [];
+const regularList = [];
 
 export const counterSlice = createSlice({
   initialState : stateList.all,
   name: "counter",
   reducers: {
-    add: (state, action) => {
+    addToHotList: (state, action) => {
       console.log(action.payload)
       const memId = action.payload.mem.id;
       
@@ -154,8 +89,22 @@ export const counterSlice = createSlice({
       } else hotList.push(action.payload.mem)
       return state = {stateList:{all:[...stateList.all],regular:[...stateList.regular],hot:[...stateList.hot,...hotList]}}
     },
+    addToRegularList: (state, action) => {
+      console.log(action.payload)
+      const memId = action.payload.mem.id;
+      
+
+      let isAddedToHotList = regularList.filter((z) => z.id === memId)
+      if(isAddedToHotList .length > 0) {
+        hotList
+      } else hotList.push(action.payload.mem)
+      return state = {stateList:{all:[...stateList.all],regular:[...stateList.regular],hot:[...stateList.hot,...regularList]}}
+    },
+    clearHotList: (state, action) => {
+      return state = {stateList:{all:[...stateList.all],regular:[...stateList.regular],hot:[]}}
+    },
   },
 });
 
-export const { add,remove } = counterSlice.actions;
+export const { addToHotList,addToRegularList,clearHotList } = counterSlice.actions;
 export default counterSlice.reducer;
