cb743b0c2d9a898ad4f9e478f4e1a5291dd168e9
TomcikKometa <tomcik48_84@o2.pl>
2024-07-08 09:57:28 +0200

    change text to display

--- src/app/pages/hot/page.jsx
+++ src/app/pages/hot/page.jsx
@@ -5,11 +5,12 @@ import MemList from "../../components/mem-card/MemList";
 
 export default function HotMems() {
   let  showMemList =  useStore((state) => state.mems);
-  // const showMemLists =  useSelector((state) => state.mems.rootReducer.stateList.StateList);
+  const textDataOne = 'Jesteś na liście "Hot"" i trafiają tu memy jeśli';
+  const textDataTwo = 'upvote - downvote > 5';
   
   let x = showMemList.getState()
-  console.log(x.memRootReducer.stateList.stateList.hot)
-  return <MemList memList={x.memRootReducer.stateList.stateList.hot}/>;
+  console.log(x);
+  return <MemList memList={x.memRootReducer.stateList.stateList.hot} textDataOne={textDataOne} textDataTwo={textDataTwo}/>;
   
 }
 
