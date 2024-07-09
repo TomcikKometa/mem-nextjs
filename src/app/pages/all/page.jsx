a4047c5b29e29c82ff8aeff266d6bb3c013693df
TomcikKometa <tomcik48_84@o2.pl>
2024-07-09 02:02:42 +0200

    refacotr pages

--- src/app/pages/all/page.jsx
+++ src/app/pages/all/page.jsx
@@ -1,77 +1,9 @@
 "use client";
-import { useState } from "react";
+import { useStore } from "react-redux";
 import MemList from "../../components/mem-card/MemList";
 
-const memList = [
-  {
-    title: "Mem 1",
-    upvotes: 0,
-    downvotes: 0,
-    img: "1.jpg",
-    id:0
-  },
-  {
-    title: "Mem 2",
-    upvotes: 0,
-    downvotes: 0,
-    img: "2.jpg",
-    id:1
-  },
-  {
-    title: "Mem 3",
-    upvotes: 0,
-    downvotes: 0,
-    img: "3.jpg",
-    id:2
-  },
-  {
-    title: "Mem 4",
-    upvotes: 0,
-    downvotes: 0,
-    img: "4.jpg",
-    id:3
-  },
-  {
-    title: "Mem 5",
-    upvotes: 6,
-    downvotes: 0,
-    img: "5.jpg",
-    id:4
-  },
-  {
-    title: "Mem 6",
-    upvotes: 0,
-    downvotes: 0,
-    img: "6.jpg",
-    id:5
-  },
-  {
-    title: "Mem 7",
-    upvotes: 0,
-    downvotes: 0,
-    img: "7.jpg",
-    id:6
-  },
-  {
-    title: "Mem 8",
-    upvotes: 0,
-    downvotes: 0,
-    img: "8.jpg",
-    id:7
-  },
-  {
-    title: "Mem 9",
-    upvotes: 0,
-    downvotes:0,
-    img: "9.jpg",
-    id:8
-  },
-];
-
-
 export default function AllMems() {
-
   const textDataOne = 'Głosuj już dziś i';
   const textDataTwo = 'graj o Fiata 126p oraz zestaw modnych firanek';
-  return <MemList memList={memList} textDataOne={textDataOne} textDataTwo={textDataTwo}/>;
+  return <MemList  textDataOne={textDataOne} textDataTwo={textDataTwo}/>;
 }
--- src/app/pages/hot/page.jsx
+++ src/app/pages/hot/page.jsx
@@ -1,16 +1,11 @@
 "use client";
-import store from "../../store/store";
-import { useStore,useSelector } from "react-redux";
+import { useStore } from "react-redux";
 import MemList from "../../components/mem-card/MemList";
 
 export default function HotMems() {
-  let  showMemList =  useStore((state) => state.mems);
   const textDataOne = 'Jesteś na liście "Hot"" i trafiają tu memy jeśli';
   const textDataTwo = 'upvote - downvote > 5';
-  
-  let x = showMemList.getState()
-  console.log(x);
-  return <MemList memList={x.memRootReducer.stateList.stateList.hot} textDataOne={textDataOne} textDataTwo={textDataTwo}/>;
+  return <MemList textDataOne={textDataOne} textDataTwo={textDataTwo}/>;
   
 }
 
