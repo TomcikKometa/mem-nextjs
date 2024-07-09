3b06bcb83f222e7bff780ec848568fac0c3f8d10
TomcikKometa <tomcik48_84@o2.pl>
2024-07-09 11:44:07 +0200

    change render text

--- src/app/components/mem-card/MemList.jsx
+++ src/app/components/mem-card/MemList.jsx
@@ -31,7 +31,7 @@ export default function MemList({ textDataTwo, textDataOne }) {
   return (
     <div className="p-4 mx-auto lg:max-w-6xl md:max-w-4xl sm:max-w-full z-10">
       <div className="flex p-8 flex-col items-center justify-between text-3xl overflow-hidden">
-        <h2>{textDataOne}</h2>
+        <p className="text-4xl mb-4">{textDataOne}</p>
         <p>{textDataTwo}</p>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
